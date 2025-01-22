87f0f30301c73739757919caa59124de62a33017
Scarlett Danger <scarlett.danger@aol.com>
2025-01-22 01:24:25 -0600

    updating imports of more functions

--- triggers/refresh_active_views_once.ts
+++ triggers/refresh_active_views_once.ts
@@ -1,4 +1,4 @@
-import { Trigger } from "deno-slack-sdk/types.ts";
+import type { Trigger } from "deno-slack-sdk/types.ts";
 import { TriggerTypes } from "deno-slack-api/mod.ts";
 import workflow from "../workflows/active_view_refresher_once.ts";
 
